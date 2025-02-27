f200b6d1f80b2439acc8fcc09ceb954206dadcff
Gregory Pereverzev <boot762@gmail.com>
2025-02-27 22:23:20 +0300

    feat: add header to App.vue

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="p-10 h-[100vh] bg-teal-600 overflow-auto">
-    <NuxtRouteAnnouncer />
+    <!-- <NuxtRouteAnnouncer /> -->
+    <h1 class="text-4xl text-white flex items-center mb-10">Kanban Board</h1>
     <KanbanBoard />
   </div>
 </template>
